6ea378cbad5d533d56e24a2a7da94432b77423d4
yaobiao <17608439620@163.com>
2019-06-11 18:07:51 +0800

    删除文件

--- SpringBoot_Jdbc/src/main/java/com/eastcom/service/DeptService.java
+++ SpringBoot_Jdbc/src/main/java/com/eastcom/service/DeptService.java
@@ -2,29 +2,29 @@ package com.eastcom.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eastcom.sql.bean.Dept;
-import com.eastcom.sql.dao.IDeptDao;
+import com.eastcom.sql.dao.DeptDao;
 
-@Service
+//@Service
 public class DeptService {
-	
+	/*
 	@Autowired
-	private IDeptDao deptDao;
+	private DeptDao deptDao;
 	
 	public Dept selectById(Integer id) {
 		return deptDao.selectById(id);
 	} 
-	
 	public Integer insert(Dept dept) {
 		return deptDao.insert(dept);
 	}
-	
-	public Integer updateById(Integer id,Dept dept) {
-		return deptDao.updateById(id,dept);
-	}
-	
-	public Integer deleteById(Integer id) {
-		return deptDao.deleteById(id);
-	}
-}
+	*/
+/*//	public Integer updateById(Integer id,Dept dept) {
+//		return deptDao.updateById(id,dept);
+//	}
+//	
+//	public Integer deleteById(Integer id) {
+//		return deptDao.deleteById(id);
+//	}
+*/}
